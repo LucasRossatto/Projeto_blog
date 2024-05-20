af3d6fb37723606ca003eee6e81e2d4d1e492896
LucasRossatto <lucas.sant.anagn3@gmail.com>
2024-05-20 16:31:57 -0300

    1.2 add post

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+const comments = document.getElementById("comments");
+const like = document.getElementById("like");
 
+const initialImage = "style/images/como.png"
+const alternateImage = "style/images/file.png"
+
+like.addEventListener("click", function () {
+    const likeImage = like.querySelector('img');
+    if (likeImage.src.includes(initialImage)) {
+        likeImage.src = alternateImage;
+    } else {
+        likeImage.src = initialImage;
+    }
+})
 fetch("10.92.198.38:8080/feed/posts", {
     method: "Post",
     body: userData,
